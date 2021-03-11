cd8bd5293fadb8022f1aea9b6d0715c91f6e870f
Nicole <ngasperini@ussilica.com>
2021-03-11 10:49:01 -0600

    Import useState and map through aggregated brewery data

--- src/Brewery/Brewery.js
+++ src/Brewery/Brewery.js
@@ -1,17 +1,22 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import styles from './Brewery.module.scss';
 import BreweryCard from './BreweryCard/BreweryCard';
 import { getBreweries } from '../BreweryAPI/BreweryAPI';
 
 const Brewery = () => {
-
+    const [breweries, setBreweries] = useState({});
 
     useEffect(() => { 
         const getBrewery = async () => {
             const apiResponse = await getBreweries();
-            const aggregatedData = apiResponse.map(response => {
-                return response;
+            const aggregatedData = apiResponse.map(brewery => {
+                return {
+                    id: brewery.id,
+                    title: brewery.name,
+                    description: brewery.description
+                };
             })
+            setBreweries(aggregatedData);
         }
         
         getBrewery();
